666a8a47b6cfec0bd21554fdf50e108f3a9f1be1
bshardi <bart.hardison@gmail.com>
2014-02-12 17:39:03 -0500

    adding about and contact pages and setting up active class on menu

--- client/views/includes/header.js
+++ client/views/includes/header.js
@@ -1,5 +1,16 @@
 Template.header.helpers({
 	isAdminUser: function(){
 		return Roles.userIsInRole(Meteor.user(), ["admin"]);
+	},
+	activeRouteClass: function(/* route names */) {
+	    var args = Array.prototype.slice.call(arguments, 0);
+	    args.pop();
+
+	    var active = _.any(args, function(name) {
+	      return Router.current().route.name === name
+	    });
+
+	    return active && 'active';
 	}
+
 });
